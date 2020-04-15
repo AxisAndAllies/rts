929bab2d5b3680c0b9c1d7570da554caa14a21f0
cktang88 <arctangent20@yahoo.com>
2020-04-14 20:52:03 -0500

    buff big units b/c already disadvantaged w/ long build time

--- game/util.js
+++ game/util.js
@@ -7,16 +7,22 @@ const animals = require("./lib/animals");
 
 function calcCost(obj) {
   let { dmg, health, range, speed, reload, turn, accuracy } = obj;
-  let coverage = Math.pow(speed + range * Math.sqrt(turn) * 4, 1.5); // b/c of kiting
+  let coverage = Math.pow(speed * 5 + range * Math.sqrt(turn) * 4, 1.5); // b/c of kiting
 
   // accuracy is more important as damage increases/reload increases
-  let efficient_dps = (dmg / reload) * ((accuracy / 100) * 0.75 + 0.25);
+  let efficient_dps =
+    Math.pow(Math.sqrt((dmg + 2) / reload), 0.8) *
+    ((accuracy / 100) * 0.75 + 0.25);
 
   // speed^2 to correct for value of moving fast
   // dps * health = damage output over lifespan
   // sqrt(turn) b/c difference between 5 and 10 deg way more valuable than 180 to 360 deg.
   let cost =
-    coverage * efficient_dps * (health + speed * 2) * Math.sqrt(health) * 0.004;
+    coverage *
+    efficient_dps *
+    (dmg + health + speed * 2) *
+    Math.sqrt(health + 2) *
+    0.002;
   cost = Math.max(cost, 1000);
   return Math.round(cost);
 }
--- test.js
+++ test.js
@@ -1,8 +1,51 @@
-// const paper = require("paper");
-// const { Point } = paper;
+/**
+ * Run with `node test.js`
+ */
 
-// a = new paper.Point(20, 20);
-// b = new paper.Point(30, 10);
-// c = b - a;
-// console.log(c);
-// console.log(a - 2);
+const { calcCost } = require("./game/util");
+
+const testunits = [
+  {
+    dmg: 9,
+    health: 1,
+
+    range: 200,
+    speed: 5,
+
+    reload: 3,
+    turn: 15,
+    accuracy: 90,
+  },
+  {
+    dmg: 2,
+    health: 30,
+
+    range: 90,
+    speed: 20,
+
+    reload: 2,
+    turn: 30,
+    accuracy: 60,
+  },
+  {
+    dmg: 3,
+    health: 6,
+
+    range: 70,
+    speed: 25,
+
+    reload: 6,
+    turn: 25,
+    accuracy: 50,
+  },
+];
+
+const costs = [18811, 70953, 11549];
+
+let testCalcCost = () => {
+  console.assert(calcCost(testunits[0]) === costs[0]);
+  console.assert(calcCost(testunits[1]) === costs[1]);
+  console.assert(calcCost(testunits[2]) === costs[2]);
+};
+
+testCalcCost();
